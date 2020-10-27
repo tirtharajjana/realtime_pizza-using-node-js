b4bb111db74e0ff02eb679cccd5c80d8e344ef31
tirtharajjana <tirtharajjana615@gmail.com>
2020-10-27 08:18:44 +0530

    'make_home_page'

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express=require("express")
 const app = express();
 const ejs=require('ejs');
 const path=require("path")
 const expressLayout=require("express-ejs-layouts")
 const PORT = process.env.PORT || 3300
-
-
+/**
+ * assets
+ */
+app.use(express.static("public"))
 app.get('/',(req,res)=>{
     res.render('home')
 })
