804da920d00120b3ec60be1932e717879e5796e8
tirtharajjana <tirtharajjana615@gmail.com>
2020-10-28 08:39:45 +0530

    add cart page and auth

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const PORT = process.env.PORT || 3300
  * assets
  */
 app.use(express.static("public"))
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+
 
 /**
  * set templates
@@ -18,6 +16,25 @@ app.get('/',(req,res)=>{
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set("view engine",'ejs')
+
+
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
 
 
 
